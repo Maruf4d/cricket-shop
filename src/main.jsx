e0a4fa82e600c15ff7d47cda88e8c47d7df860fa
Maruf4d <marufjava@gmail.com>
2024-05-17 23:58:10 +0600

    create routes section

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { router } from './Routes/routes.jsx'
+
 import { RouterProvider } from 'react-router-dom'
+import { router } from './Routes/routes'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
